f71657e5889eaece86b26b584cda8d579108edcf
Sachin Sharma <79436926+devsachin007@users.noreply.github.com>
2023-01-21 14:53:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import { Routes, Route} from "react-router-dom";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 import Contact from "./components/Contact";
 import {Home} from "./components/Home";
 
 function App() {
   return<>
+    <BrowserRouter basename="devapp">
     <Routes>
-      <Route exact path="/devapp" element={<Home />} />
-      <Route path="/devapp/contact" element={<Contact />} />
+      <Route exact path="/" element={<Home />} ></Route>
+      <Route path="/contact" element={<Contact />} ></Route>
     </Routes>
+      </BrowserRouter>
     </>
 }
 
